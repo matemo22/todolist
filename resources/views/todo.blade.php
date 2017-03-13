db1efaff691d74ada81b3981601c5d20f2ee66c2
matemo22 <matemo2204@gmail.com>
2017-03-13 11:23:14 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>ToDo List</h1>
+				<ul>
+					<li>Taki</li>
+					<li>Mitsuha</li>
+					<li>Yotsuha</li>
+				</ul>
+				<form>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="form-group">
+								<input type="text" name="todo_item" class="form-control" placeholder="Item baru...">
+							</div>
+						</div>
+						<div class="col-lg-1">
+							<button type="submit" class="btn btn-success">Add</button>
+						</div>
+						<div class="col-lg-8">
+						</div>
+					</div>
+				</form>
+				<script type="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
